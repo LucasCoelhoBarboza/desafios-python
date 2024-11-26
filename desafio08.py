61db76ddcff515ff47413eedf36606c308ccf23b
LucasCoelhoBarboza <lucs.c0elh@gmail.com>
2024-11-26 11:22:53 -0300

    Desafio Nº8

---
+++ desafio08.py
@@ -0,0 +1,7 @@
+s = input("Digite F ou M: ").upper()
+if s == "F":
+    print("O genero é feminino!")
+if s == "M":
+    print("O genero é masculino!")
+if s != "F" and s != "M":
+    print("Sexo invalido!")
