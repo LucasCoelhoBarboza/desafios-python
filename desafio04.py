e7806e11ce2cbb760ff9196591fc951313f15c9f
LucasCoelhoBarboza <lucs.c0elh@gmail.com>
2024-11-26 11:22:03 -0300

    Desafio Nº4

---
+++ desafio04.py
@@ -0,0 +1,6 @@
+n1 = float(input("Informe a primeira nota: "))
+n2 = float(input("Informe a segunda nota: "))
+n3 = float(input("Informe a terceira nota: "))
+n4 = float(input("Informe a quarta nota: "))
+rest = (n1 + n2 + n3 + n4) / 4
+print("Sua média é: {}".format(rest))
